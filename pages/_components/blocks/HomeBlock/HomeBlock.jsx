15af1c60c7cde0b499871005d067d8c7d8d6dfcc
Abror <mmtklvabrr@gmail.com>
2022-06-14 13:39:37 +0600

    gcnfgggggg

--- pages/_components/blocks/HomeBlock/HomeBlock.jsx
+++ pages/_components/blocks/HomeBlock/HomeBlock.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
 import nasway from './HomeBlock.module.scss'
 import Title from '../../fabric/Title/Title';
 
 import Mototototototototototo from './Moto.png'
 import SubText from '../../fabric/SubText/SubText';
-
+import Image from 'next/image';
 
 const HomeBlock = () => {
     return (
         <div className={nasway.HomeBlock}>
-            <div className={nasway.ImgBlock}>
-                <Image src={Mototototototototototo.src} alt="МОТОШКОЛА “STREET BIKE”В РОСТОВЕ-НА-ДОНУ" width={"100%"} height={"100%"} layout='responsive'/>
-            </div>            
+            <div className={nasway.ImageBlock}>
+                <Image src={Mototototototototototo.src} alt="dsdddd" width={672} height={567}/>    
+            </div>     
             <div className={nasway.TxtBlock}>
                 <Title green content="МОТОШКОЛА “STREET BIKE”"/>
                 <Title content="В РОСТОВЕ-НА-ДОНУ"/>
