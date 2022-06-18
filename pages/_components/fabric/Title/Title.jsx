2baa57e5d4bfb2385176a4e431e5ec91e9fe7916
Alexander <89515014507@mail.ru>
2022-06-19 00:55:10 +0600

    fixed style selection block for Title tag

--- pages/_components/fabric/Title/Title.jsx
+++ pages/_components/fabric/Title/Title.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import nasway from "./Title.module.scss";
 
+const styleProps = (props) => {
+  if(props.green) return(nasway.green);
+  if(props.black) return(nasway.black);
+  if(props.biggreen) return(nasway.biggreen);
+  if(props.errorwhite) return(nasway.errorwhite);
+
+  return(nasway.white);  
+}
+
 const Title = (props) => {
   return (
     <div>
-    <h3 className={props.green ? nasway.green :
-                   props.black ? nasway.black : 
-                   props.biggreen ? nasway.biggreen :
-                   props.errorwhite ? nasway.errorwhite : nasway.white}>
-    
+
+     <h3 className={styleProps(props)}>   
         {props.content}
-    </h3>
-  
+     </h3>
 
     </div>
   );
