792f42adffa7c5fb4dc091f4e01a018681472691
Abror <mmtklvabrr@gmail.com>
2022-06-19 12:18:44 +0600

    afdeds

---
+++ pages/order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Order from './_components/order/Order';
+import Header from './_components/components/header/Header';
+
+const order = () => {
+    return (
+        <div>
+            <Header/>
+            <Order/>
+        </div>
+    );
+};
+
+export default order;
