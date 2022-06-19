748538b9e94368f15ead9a547e38cb3cfd9e1624
Abror <mmtklvabrr@gmail.com>
2022-06-19 19:06:31 +0600

    wefwef

--- pages/_components/blocks/Content.jsx
+++ pages/_components/blocks/Content.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import nasway from './Content.module.scss'
+import React from "react";
+import nasway from "./Content.module.scss";
 
-import HomeBlock from './HomeBlock/HomeBlock';
-import Motopark from './Motopark/Motopark';
-import PhotoZone from './PhotoZone/PhotoZone';
-import Documents from './Documents/Documents';
-import Questions from './Questions/Questions';
+import HomeBlock from "./HomeBlock/HomeBlock";
+import Motopark from "./Motopark/Motopark";
+import PhotoZone from "./PhotoZone/PhotoZone";
+import Documents from "./Documents/Documents";
+import Questions from "./Questions/Questions";
+import Service from "./Service/Service";
 
 const Content = () => {
-    return (
-        <div className={nasway.Content}>
-            <HomeBlock/>
-            <Motopark/>   
-              {/* Arrmor_Zone */}
-              <PhotoZone/>
-              <Documents/>
-              <Questions/>
-              
-
-
-        </div>
-    );
+  return (
+    <div className={nasway.Content}>
+      <HomeBlock />
+      <Motopark />
+      <Service/>
+      {/* Arrmor_Zone */}
+      <PhotoZone />
+      <Documents />
+      <Questions />
+    </div>
+  );
 };
 
 export default Content;
--- pages/_components/fabric/RegularButton/RegButton.jsx
+++ pages/_components/fabric/RegularButton/RegButton.jsx
@@ -3,7 +3,7 @@ import { Button } from "@mui/material";
 import nasway from "./RegButton.module.scss";
 
 const RegButton = (props) => {
-  return <Button className={props.GreyStyle ? nasway.GreyStyle : nasway.BuStyle}>{props.text}<div></div></Button>;
+  return <Button className={props.GreyStyle ? nasway.GreyStyle : nasway.BuStyle}>{props.text}</Button>;
 };
 
 export default RegButton;
