4c4e8d72088cd3188dbd62fd3011a71dd8acfada
abror-mqv <mmtklvabrr@gmail.com>
2022-07-13 01:27:08 +0500

    Swiper worked

--- pages/_components/articles/Articles.jsx
+++ pages/_components/articles/Articles.jsx
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import RegButton from "../fabric/RegularButton/RegButton";
 import Card from "./Card";
 import nasway from "./Articles.module.scss";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 
 const Articles = () => {
   const [cat, setCat] = useState(1);
@@ -25,20 +35,33 @@ const Articles = () => {
     };
     fetchData();
   }, []);
-  const handleSwitch = (ar) => console.log("HERO", ar);
-  console.log("STATI ",data[cat-1].attributes.statis.data[0])
+  console.log("SSSTR", data[cat - 1]?.attributes?.statis.data);
   return (
     <section className={nasway.Article}>
       <div className={nasway.Categories}>
         <article>
-          {
-            data[cat-1].attributes.statis.data.map(el=>{
-              return(
-                <p>{el.attributes.PreviewTitle}</p>
-              )
-            })
-          }
+          {data[cat - 1]?.attributes?.statis.data.map((el) => {
+            return (
+              <div className={nasway.SwiperSlide}>
+                <h2>{el.attributes.PreviewTitle}</h2>
+              </div>
+            );
+          })}
         </article>
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+
+        </Swiper>
         <ul>
           {data.map((el) => {
             if (cat == el.id) {
